f30345474bb89175248abcc00366b59c82c07350
PabloProst <pabloprost16@gmail.com>
2023-12-23 15:00:38 +0100

    feat: games model created

--- src/db.ts
+++ src/db.ts
@@ -5,6 +5,7 @@ import { Upgrades1703338802930 } from "./migration/1703338802930-upgrades"
 import { Games1703338906461 } from "./migration/1703338906461-games"
 import { UpgradesUsers1703339244579 } from "./migration/1703339244579-upgrades_users"
 import { User } from "./models/User"
+import { Game } from "./models/Game"
 export const AppDataSource = new DataSource({
 type: "mysql",
 host: "localhost",
@@ -13,7 +14,7 @@ username: "root",
 password: "123",
 database: "tob_backend",
 migrations:[Users1703337928564, Upgrades1703338802930, Games1703338906461, UpgradesUsers1703339244579],
-entities: [User],
+entities: [User, Game],
 synchronize: false,
 logging: false,
 })
--- src/models/Game.ts
+++ src/models/Game.ts
@@ -1,6 +1,28 @@
-import { Entity } from "typeorm"
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from "typeorm";
+import { User } from "./User";
 
-@Entity()
-export class Game {
+@Entity("games")
+export class Game extends BaseEntity {
 
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    userId!: number;
+
+    @Column()
+    score!: number;
+
+    @Column()
+    time!: number;
+
+    @Column()
+    created_at!: Date;
+
+    @Column()
+    updated_at!: Date;
+
+    @ManyToOne(() => User, user => user.id, { onDelete: "CASCADE" })
+    @JoinColumn({ name: "user_id" })
+    user!: User;
 }
