3c7630f1e08996c972121563266ca75ab3a6b3af
PabloProst <pabloprost16@gmail.com>
2023-12-23 14:58:03 +0100

    feat: User model created

--- src/db.ts
+++ src/db.ts
@@ -4,6 +4,7 @@ import { Users1703337928564 } from "./migration/1703337928564-users"
 import { Upgrades1703338802930 } from "./migration/1703338802930-upgrades"
 import { Games1703338906461 } from "./migration/1703338906461-games"
 import { UpgradesUsers1703339244579 } from "./migration/1703339244579-upgrades_users"
+import { User } from "./models/User"
 export const AppDataSource = new DataSource({
 type: "mysql",
 host: "localhost",
@@ -12,7 +13,7 @@ username: "root",
 password: "123",
 database: "tob_backend",
 migrations:[Users1703337928564, Upgrades1703338802930, Games1703338906461, UpgradesUsers1703339244579],
-entities: [],
+entities: [User],
 synchronize: false,
 logging: false,
 })
