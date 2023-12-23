02005842f7aff5243b03bafd34a7c4a371179c99
PabloProst <pabloprost16@gmail.com>
2023-12-23 18:14:46 +0100

    feat: login controller created

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -51,4 +51,68 @@ const addUser = async (req: Request, res: Response) => {
     }
   }
 
-  export { addUser }
+
+  // Login
+const login = async (req: Request, res: Response) => {
+    try {
+      const email = req.body.email;
+      const password = req.body.password;
+  
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      });
+  
+      if (!user) {
+        return res.status(400).json(
+          {
+            success: false,
+            message: `Email or password incorrect`,
+          }
+        );
+      }
+  
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(400).json(
+          {
+            success: false,
+            message: `Email or password incorrect`,
+          }
+        );
+      }
+  
+      // Generate token
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email
+        },
+        "geekshubs",
+        {
+          expiresIn: "3h",
+        }
+      );
+  
+      return res.json({
+        success: true,
+        message: `Login successful, welcome ${user.name}`,
+        user_id: user.id,
+        email: user.email,
+        name: user.name,
+        password: user.password,
+        token: token
+      });
+  
+    } catch (error) {
+      return res.status(500).json(
+        {
+          success: false,
+          message: `Error during login.`,
+          error: error
+        }
+      );
+    }
+  }
+
+  export { addUser, login }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { addUser } from "../controllers/authController";
+import { addUser, login } from "../controllers/authController";
 
 
 const router = Router()
 
 // User register
 router.post('/register', addUser);
+// User login
+router.post('/login', login);
 
 export { router }
