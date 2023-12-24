4e747a60a63505845279f5d19181744db74a63ff
PabloProst <pabloprost16@gmail.com>
2023-12-24 17:41:46 +0100

    feat: get all scores

--- src/controllers/scoreController.ts
+++ src/controllers/scoreController.ts
@@ -2,20 +2,39 @@ import { Game } from "../models/Game";
 import { Request, Response } from "express";
 
 const addScore = async (req: Request, res: Response) => {
-  try {
-    const { userId, score } = req.body;
+    try {
+      const { userId, score } = req.body;
+  
+      const game = new Game();
+      game.userId = userId;
+      game.score = score;
+  
+      await game.save();
+  
+      res.status(200).json({ message: "Score added successfully" });
+    } catch (error) {
+      console.error("Error creating score:", error);
+      res.status(500).json({ error: "Internal Server Error: " + error });
+    }
+  };
 
-    const game = new Game();
-    game.userId = userId;
-    game.score = score;
+  //Get ranking
+  const getRanking = async (req: Request, res: Response) => {
+    try {
+        const ranking = await Game.find();
 
-    await game.save();
-
-    res.status(200).json({ message: "Score added successfully" });
-  } catch (error) {
-    console.error("Error creating score:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+        console.log(ranking);
+        return res.json({
+            ranking,
+        });
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: 'Ranking cant be retrieved',
+            error: error,
+        });
+    }
   }
-};
+  
 
-export { addScore };
+export { addScore, getRanking };
--- src/routes/scoreRoutes.ts
+++ src/routes/scoreRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { addScore } from "../controllers/scoreController";
+import { addScore, getRanking } from "../controllers/scoreController";
+// import { auth } from "../middlewares/auth";
 
 
 const router = Router()
 
 
 // New score
-router.post('/score',addScore);
+router.post('/score' , addScore);
+// Get Ranking
+router.get('/allscores', getRanking)
 
 export { router }
